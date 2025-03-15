8dc8e541e818369c5c1fbd252d098f92daf7f1a6
wchwawa <wch19961116@gmail.com>
2025-03-15 16:35:27 +1100

    empty stash

--- constant/pdfTemplate.ts
+++ constant/pdfTemplate.ts
@@ -1,4 +1,5 @@
 const pdfTemplate = {
+  uuid: '123e4567-e89b-12d3-a456-426614174000',
   lectureTitle: 'Test Paper',
   abstract: 'Abstract',
   keyPoints: ['Key Point 1', 'Key Point 2', 'Key Point 3'],
