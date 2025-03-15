21436280cdebaa7ba0067bbd8ce5461ec3ffcbf9
Lantian Wei <lwei0448@uni.sydney.edu.au>
2025-03-15 11:36:58 +1100

    feat: starting page

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,115 @@
+"use client";
 import Image from "next/image";
+
+import { IconFileSmile, IconFileUpload } from "@tabler/icons-react";
+import {
+  Box,
+  Button,
+  createListCollection,
+  Portal,
+  Select,
+  Heading,
+  Text,
+  Flex,
+} from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <Box
+      bg="gray.100"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      w="100vw"
+      h="100vh"
+    >
+      <Flex flexDirection="column" alignItems="center" justifyContent="center">
+        <IconFileSmile size={64} />
+        <Heading mt={2} fontSize="4xl" fontWeight="medium">
+          Exam Paper Generator
+        </Heading>
+        <Text mt={3} color="gray.500">
+          Simply add your lecture file, we will help you to create{" "}
+        </Text>
+        <Box
+          bg="white"
+          mt={6}
+          border="1px"
+          w="650px"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          borderColor="gray.200"
+          px={4}
+          py={2}
+          borderRadius="md"
+        >
+          <Flex
+            borderBottom="1px"
+            w="full"
+            px={2}
+            color="gray.500"
+            alignItems="center"
+            h="150px"
+            justifyContent="center"
+            flexDirection="column"
+          >
+            <IconFileUpload size={42} />
+            <Text mt={2}>Add Lecture Files</Text>
+            <Text fontSize="xs" color="gray.400">
+              Accepts PDF, PPTX, Word and More
+            </Text>
+          </Flex>
+          <Flex
+            borderTop="1px solid"
+            borderColor="gray.200"
+            w="full"
+            px={2}
+            py={2}
+            justifyContent="space-between"
+          >
+            <Box w="180px">
+              <Select.Root size="md" collection={questionTypes} multiple>
+                <Select.HiddenSelect />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+                <Select.Control>
+                  <Select.Trigger>
+                    <Select.ValueText placeholder="Select Question Type" />
+                  </Select.Trigger>
+                </Select.Control>
+                <Portal>
+                  <Select.Positioner>
+                    <Select.Content>
+                      {questionTypes.items.map((item) => (
+                        <Select.Item key={item.value} item={item}>
+                          {item.label}
+                          <Select.ItemIndicator />
+                        </Select.Item>
+                      ))}
+                    </Select.Content>
+                  </Select.Positioner>
+                </Portal>
+              </Select.Root>
+            </Box>
+
+            <Button>Generate</Button>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
   );
 }
+
+const questionTypes = createListCollection({
+  items: [
+    {
+      label: "Multiple Choice",
+      value: "mcq",
+    },
+    {
+      label: "Short Answer",
+      value: "short_answer",
+    },
+  ],
+});
