c0cddcf65adbeebecace16dc1f0d9dce322b6fcb
Tong Chen <linnkid.chen@gmail.com>
2025-03-16 15:56:38 +1100

    fix bug

--- utils/openai/agentClient.ts
+++ utils/openai/agentClient.ts
@@ -1,10 +1,19 @@
 import { ChatOpenAI } from "@langchain/openai";
 
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+
 const agentClient = new ChatOpenAI({
   model: "gpt-4o",
   temperature: 0,
-  maxRetries: 3,
-  apiKey: process.env.OPENAI_API_KEY
-})
+  maxRetries: 2,
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+// const agentClient = new ChatGoogleGenerativeAI({
+//   modelName: "gemini-2.0-flash-lite",
+//   temperature: 0,
+//   maxRetries: 2,
+//   apiKey: process.env.GOOGLE_API_KEY,
+// });
 
 export default agentClient;
